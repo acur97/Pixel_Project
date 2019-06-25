a4d9c1667cfe743ad0d4031123277bd49a00f2b5
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2019-06-25 04:47:46 -0500

    fixes bala y sprites con animacion

--- Assets/_Scripts/PlayerControl.cs
+++ Assets/_Scripts/PlayerControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float velocidad;
+    public float XdevolverBala;
     public Transform cubeTest;
     public Transform pointerTest;
     public Camera cam;
@@ -13,16 +14,55 @@ public class PlayerControl : MonoBehaviour
     public List<GameObject> enemigos;
 
     private Vector3 Mpos;
+    private Vector2 moves;
     private bool balaPasando;
+    private Animator anim;
+    private SpriteRenderer Srender;
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
+        Srender = GetComponent<SpriteRenderer>();
         pointerTest.GetComponentsInChildren<Transform>()[1].localEulerAngles = new Vector3(-90, 90, 90);
     }
 
     private void Update()
     {
-        transform.position += new Vector3(Time.unscaledDeltaTime * (Input.GetAxis("Horizontal") * velocidad), Time.unscaledDeltaTime * (Input.GetAxis("Vertical") * velocidad));
+        moves = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position += new Vector3(Time.unscaledDeltaTime * (moves.x * velocidad), Time.unscaledDeltaTime * (moves.y * velocidad));
+
+        if (moves.y > 0 && moves.x == 0)
+        {
+            anim.SetBool("mov_side", false);
+            anim.SetBool("mov_up", true);
+            anim.SetBool("mov_down", false);
+        }
+        if (moves.y < 0 && moves.x == 0)
+        {
+            anim.SetBool("mov_side", false);
+            anim.SetBool("mov_up", false);
+            anim.SetBool("mov_down", true);
+        }
+        if (moves.x > 0)
+        {
+            anim.SetBool("mov_side", true);
+            Srender.flipX = false;
+            anim.SetBool("mov_up", false);
+            anim.SetBool("mov_down", false);
+        }
+        if (moves.x < 0)
+        {
+            anim.SetBool("mov_side", true);
+            Srender.flipX = true;
+            anim.SetBool("mov_up", false);
+            anim.SetBool("mov_down", false);
+        }
+        if (moves.x == 0 && moves.y == 0)
+        {
+            anim.SetBool("mov_side", false);
+            anim.SetBool("mov_up", false);
+            anim.SetBool("mov_down", false);
+        }
 
         Mpos = cam.ScreenToWorldPoint(Input.mousePosition);
         cubeTest.position = new Vector3(Mpos.x, Mpos.y);
@@ -34,6 +74,8 @@ public class PlayerControl : MonoBehaviour
             for (int i = 0; i < balas.Count; i++)
             {
                 balas[i].transform.rotation = pointerTest.rotation;
+                float vel = balas[i].GetComponent<Bala>().velocidad;
+                balas[i].GetComponent<Bala>().velocidad = vel * XdevolverBala;
             }
             for (int i = 0; i < enemigos.Count; i++)
             {
